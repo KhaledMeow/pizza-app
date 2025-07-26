9657ea8a5292c788aed5c47fc30bcc7fb3475187
KhaledMeow <hala2101983@gmail.com>
2025-07-26 23:22:17 +0300

    Organizing

--- src/App.js
+++ src/App.js
@@ -1,79 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Pizza Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
-
+import React from 'react';
+import { pizzaData } from './data/pizzas';
+import Pizza from './components/Pizza';
 
 function App() {
-    return (
-        <div>
-            <h1>Pizza App!</h1>
-            {pizzaData.map((pizza) => (
-                <Pizza key={pizza.name} name={pizza.name} ingredients={pizza.ingredients} price={pizza.price} photoName={pizza.photoName} soldOut={pizza.soldOut} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="menu">
+      <h1>Our Menu</h1>
+      <div className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza 
+            key={pizza.name} 
+            name={pizza.name} 
+            ingredients={pizza.ingredients} 
+            price={pizza.price} 
+            photoName={pizza.photoName} 
+            soldOut={pizza.soldOut} 
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-function Pizza({ name, ingredients, price, photoName, soldOut }) {
-    return (
-        <div>
-            <img src={photoName} alt={name} />
-            <h3>{name}</h3>
-            <p>{ingredients}</p>
-            <span>{soldOut ? "SOLD OUT" : price+'$'}</span>
-        </div>
-    );
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
 
 export default App;
---
+++ src/components/Pizza.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
+  return (
+    <div className={`pizza ${soldOut ? 'sold-out' : ''}`}>
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{soldOut ? 'SOLD OUT' : `$${price}`}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Pizza;
